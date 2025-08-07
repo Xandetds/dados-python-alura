1617767d9044e62266ae0ea20e1e8ce393c1e6c6
Alexandre Tibes <Alexandretibes9@gmail.com>
2025-08-07 16:07:03 -0300

    feat: Adicionado exercício de criação de gráfico de rosca com Plotly

---
+++ dados_visuais/exercicio06.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt 
+import seaborn as sns
+import plotly.express as px
+
+df = pd.read_csv("https://raw.githubusercontent.com/guilhermeonrails/data-jobs/refs/heads/main/salaries.csv")
+
+df_limpo = df.dropna()
+
+df_limpo.isnull().sum()
+
+print(df_limpo.head())
+
+
+remoto_contagem = df_limpo["remote_ratio"].value_counts().reset_index()
+remoto_contagem.columns = ["tipo_tabalho", "quantidade"]
+
+fig = px.pie(remoto_contagem,
+            names= "tipo_trabaalho",
+            values= "quantidade",
+            title= "Proporção dos tipos de trabalho",
+            hole= 0.5 #vai de pizza pra rosca
+            )
+
+fig.update_traces(textinfo= "percent+label")
+fig.show()
+
